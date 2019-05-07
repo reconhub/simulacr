8a181f6886469ba5b74952240278284b182ea33a
Thibaut <thibautjombart@gmail.com>
2019-05-07 12:27:43 +0100

    new function to test spread functions

---
+++ R/assert_spread_function.R
@@ -0,0 +1,29 @@
+#' Test that a 'spread' function is correct
+#'
+#' Internal. Checks that the input is a function, and takes a single argument.
+#'
+#' @noRd
+
+assert_spread_function(x)  <- function(x) {
+  ## check function type first
+  if (!is.function(x)) {
+    msg <- sprintf(
+        "`x` is not a function but a `%s`)",
+        class(x)[1])      
+    stop(msg)
+    
+  }
+
+
+  ## check that it takes a single argument
+  arguments <- as.list(args(x))
+  arguments <- arguments[!vapply(arguments, is.null, logical(1))] # need to remove the last `NULL`
+
+  if (length(arguments) > 1) {
+    msg <- "if `x` is a function, it should accept a single argument `n`"
+    stop(msg)
+  }
+
+  invisible(NULL)
+}
+
