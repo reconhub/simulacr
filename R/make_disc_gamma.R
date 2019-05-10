d92eaf3fa72fff1ed7a58f3ec9ad16dcd489427d
Thibaut <thibautjombart@gmail.com>
2019-05-10 14:47:37 +0100

    adding make_disc_gamma

---
+++ R/make_disc_gamma.R
@@ -0,0 +1,35 @@
+#' Make a discretized gamma distribution
+#' 
+#' This function is a simple wrapper around the packages *epitrix* and
+#' *distcrete* allowing to create a discretised Gamma distribution with given
+#' mean and standard deviration, returning a `distcrete` object.
+#'
+#' @param mean the mean of the distribution
+#'
+#' @param sd the standard deviation of the distribution
+#'
+#' @author Thibaut Jombart
+#'
+#' @examples
+#'
+#' serial_interval <- make_disc_gamma(15, 9)
+#'
+#'
+#' ## check PMF
+#' plot(0:30, serial_interval$d(0:30), type = "h",
+#'      lwd = 3, col = "navy",
+#'      xlab = "Number of days", ylab = "probability",
+#'      main = "Probability mass function")
+#'
+#' ## simulate random numbers
+#' serial_interval$d(20)
+#' 
+
+make_disc_gamma <- function(mean, sd) {
+  params <- epitrix::gamma_mucv2shapescale(mean, sd / mean)
+  distcrete::distcrete("gamma",
+                       interval = 1,
+                       w = 0,
+                       shape = params$shape,
+                       scale = params$scale)
+}
